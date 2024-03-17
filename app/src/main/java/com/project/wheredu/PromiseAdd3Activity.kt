a780df9b9ca5c5e21d6cc02c0b88278a26ca9c08
MyLittleRengar <yjy8860@naver.com>
2024-03-17 23:30:49 +0900

    약속 화면 캘린더 디자인, 약속 추가 화면 추가, 디자인 추가

---
+++ app/src/main/java/com/project/wheredu/PromiseAdd3Activity.kt
@@ -0,0 +1,11 @@
+package com.project.wheredu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PromiseAdd3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_promise_add3)
+    }
+}
